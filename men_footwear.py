693542c11e3798b9f712438df310115b8fc9d0cd
Arish Bhayani <42268790+arish14@users.noreply.github.com>
2023-09-07 17:39:39 -0400

    Adding traning code for men footwear category

---
+++ men_footwear.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+from keras import applications
+from sklearn.metrics import pairwise_distances
+import requests
+from PIL import Image
+import pickle
+from datetime import datetime
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import plotly.express as px
+import urllib3
+urllib3.disable_warnings()
+import certifi
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+certifi.where()
+
+#import streamlit as st
+#use the below library while displaying the images in jupyter notebook
+from IPython.display import display, Image
+
+
+fashion_df = pd.read_csv("/Users/arishbhayani/Desktop/Capstone/data/New_DF.csv")
+
+print(fashion_df)
+
+
+print("Total number of products : ", fashion_df.shape[0])
+print("Total number of unique subcategories : ", fashion_df["SubCategory"].nunique())
+print("Total number of unique gender types : ", fashion_df["Gender"].nunique())
+print("Total number of unique Rating : ", fashion_df["rating"].nunique())
+
+
+url = "https://storage.googleapis.com/tensorflow/keras-applications/resnet/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5"
+response = requests.get(url, verify=False)
+
+img_width, img_height = 224, 224
+
+#top_model_weights_path = 'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+train_data_dir = "/Users/arishbhayani/Desktop/Capstone/data/Footwear/Men/Images/"
+
+nb_train_samples = 813
+epochs = 50
+batch_size = 1
+
+def men_footwear_extract_features():
+    Itemcodes = []
+    datagen = ImageDataGenerator(rescale=1. / 255)
+    model = applications.ResNet50(include_top=False, weights='imagenet')
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    for i in generator.filenames:
+        Itemcodes.append(i[(i.find("/")+1):i.find(".")])
+    extracted_features = model.predict_generator(generator, nb_train_samples // batch_size)
+    extracted_features = extracted_features.reshape((813, 100352))
+    
+    np.save(open('./Men_ResNet_features.npy', 'wb'), extracted_features)
+    np.save(open('./Men_ResNet_feature_product_ids.npy', 'wb'), np.array(Itemcodes))
+    
+a = datetime.now()
+men_footwear_extract_features()
+print("Time taken in feature extraction", datetime.now()-a)
+
+
+
+
+
