4451c6a1878ea8933dbe50ac6713ffa46c6a4a3f
Arish Bhayani <42268790+arish14@users.noreply.github.com>
2023-11-08 10:41:50 -0500

    Added rating condition to the model

--- men_footwear.py
+++ men_footwear.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from tensorflow import keras
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dropout, Flatten, Dense
-from keras import applications
+#from tensorflow import keras
+#from keras.preprocessing.image import ImageDataGenerator
+#from keras.models import Sequential
+#from keras.layers import Dropout, Flatten, Dense
+#from keras import applications
 from sklearn.metrics import pairwise_distances
 import requests
 from PIL import Image
@@ -24,57 +24,50 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 certifi.where()
 
-#import streamlit as st
-#use the below library while displaying the images in jupyter notebook
-from IPython.display import display, Image
-
-
 fashion_df = pd.read_csv("/Users/arishbhayani/Desktop/Capstone/data/New_DF.csv")
 
-print(fashion_df)
+boys_extracted_features = np.load('/Users/arishbhayani/Desktop/Capstone/Website/Model Features/Boys_ResNet_features.npy')
+boys_Productids = np.load('/Users/arishbhayani/Desktop/Capstone/Website/Model Features/Boys_ResNet_feature_product_ids.npy')
+girls_extracted_features = np.load('/Users/arishbhayani/Desktop/Capstone/Website/Model Features/Girls_ResNet_features.npy')
+girls_Productids = np.load('/Users/arishbhayani/Desktop/Capstone/Website/Model Features/Girls_ResNet_feature_product_ids.npy')
+men_extracted_features = np.load('/Users/arishbhayani/Desktop/Capstone/Website/Model Features/Men_ResNet_features.npy')
+men_Productids = np.load('/Users/arishbhayani/Desktop/Capstone/Website/Model Features/Men_ResNet_feature_product_ids.npy')
+women_extracted_features = np.load('/Users/arishbhayani/Desktop/Capstone/Website/Model Features/Women_ResNet_features.npy')
+women_Productids = np.load('/Users/arishbhayani/Desktop/Capstone/Website/Model Features/Women_ResNet_feature_product_ids.npy')
+fashion_df["ProductId"] = fashion_df["ProductId"].astype(str)
 
-
-print("Total number of products : ", fashion_df.shape[0])
-print("Total number of unique subcategories : ", fashion_df["SubCategory"].nunique())
-print("Total number of unique gender types : ", fashion_df["Gender"].nunique())
-print("Total number of unique Rating : ", fashion_df["rating"].nunique())
-
-
-url = "https://storage.googleapis.com/tensorflow/keras-applications/resnet/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5"
-response = requests.get(url, verify=False)
-
-img_width, img_height = 224, 224
-
-#top_model_weights_path = 'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
-train_data_dir = "/Users/arishbhayani/Desktop/Capstone/data/Footwear/Men/Images/"
-
-nb_train_samples = 813
-epochs = 50
-batch_size = 1
-
-def men_footwear_extract_features():
-    Itemcodes = []
-    datagen = ImageDataGenerator(rescale=1. / 255)
-    model = applications.ResNet50(include_top=False, weights='imagenet')
-    generator = datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-    for i in generator.filenames:
-        Itemcodes.append(i[(i.find("/")+1):i.find(".")])
-    extracted_features = model.predict_generator(generator, nb_train_samples // batch_size)
-    extracted_features = extracted_features.reshape((813, 100352))
+def get_similar_products_cnn(product_id, num_results):
+    Productids = []
+    if fashion_df[fashion_df['ProductId'] == product_id]['Gender'].values[0] == "Boys":
+        extracted_features = boys_extracted_features
+        Productids = boys_Productids
+    elif fashion_df[fashion_df['ProductId'] == product_id]['Gender'].values[0] == "Girls":
+        extracted_features = girls_extracted_features
+        Productids = girls_Productids
+    elif fashion_df[fashion_df['ProductId'] == product_id]['Gender'].values[0] == "Men":
+        extracted_features = men_extracted_features
+        Productids = men_Productids
+    elif fashion_df[fashion_df['ProductId'] == product_id]['Gender'].values[0] == "Women":
+        extracted_features = women_extracted_features
+        Productids = women_Productids
     
-    np.save(open('./Men_ResNet_features.npy', 'wb'), extracted_features)
-    np.save(open('./Men_ResNet_feature_product_ids.npy', 'wb'), np.array(Itemcodes))
+    if len(Productids) == 0:
+        # Default value in case no condition matches
+        Productids = []
     
-a = datetime.now()
-men_footwear_extract_features()
-print("Time taken in feature extraction", datetime.now()-a)
-
-
-
-
-
+    doc_id = np.where(Productids == product_id)[0][0]
+    pairwise_dist = pairwise_distances(extracted_features, extracted_features[doc_id].reshape(1, -1))
+    indices = np.argsort(pairwise_dist.flatten())[0:num_results]
+    
+    # Sort the indices based on 'rating' in descending order
+    sorted_indices = sorted(indices, key=lambda x: fashion_df['rating'].iloc[x], reverse=True)
+    
+    # Input product details
+    ip_row = fashion_df[['ImageURL', 'ProductTitle', 'price', 'rating']].loc[
+        fashion_df['ProductId'] == Productids[sorted_indices[0]]]
+    
+    # Similar products sorted by 'rating' and filter out products with a rating of 0
+    sim_rows = fashion_df[['ImageURL', 'ProductTitle', 'price', 'rating']].loc[
+        (fashion_df['ProductId'].isin(list(np.array(Productids)[sorted_indices]))) & (fashion_df['rating'] > 0)]
+    
+    return ip_row, sim_rows
